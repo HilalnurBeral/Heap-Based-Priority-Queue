52257170c27981f4356b1dcc1118302a978b4ca1
HilalnurBeral <67428396+HilalnurBeral@users.noreply.github.com>
2020-06-25 19:32:16 +0300

    Update Tester.java

--- ALGORITHMHW5/src/Tester.java
+++ ALGORITHMHW5/src/Tester.java
@@ -7,8 +7,7 @@ import java.util.ArrayList;
 
 //-----------------------------------------------------
 // Title: Tester class 
-// Author:Hilalnur Beral ID:11549136358
-//Author2:Ýrem Karpat ID:61234117544
+// Author:Hilalnur Beral 
 // Section: 01
 // Assignment: 5
 // Description: This class is implemented in order to test our methods..
